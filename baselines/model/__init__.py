a2c8bf2b8ed2d432f502f5fbd47d22b0368abcbb
liubinbin <1650476944@qq.com>
2022-01-05 14:17:55 +0800

    fix model load bug

--- baselines/model/__init__.py
+++ baselines/model/__init__.py
@@ -2,4 +2,3 @@ from .pointnet import PointNetCls, feature_transform_reguliarzer
 from .pointnet2 import PointNet2ClsSsg
 from .dgcnn import DGCNN
 from .pointconv import PointConvDensityClsSsg
-from ...represent_learning.cpc import CPC
