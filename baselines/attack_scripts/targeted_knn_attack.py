07608803f8626459c23227f257bf7337d1bb1e77
lbb97 <8788287+lbb97@user.noreply.gitee.com>
2022-01-09 08:15:45 +0000

    update

--- baselines/attack_scripts/targeted_knn_attack.py
+++ baselines/attack_scripts/targeted_knn_attack.py
@@ -4,26 +4,24 @@ import os
 from tqdm import tqdm
 import argparse
 import numpy as np
-import psutil
 
 import torch
 import torch.backends.cudnn as cudnn
-from torch.utils.data import DataLoader
 import torch.distributed as dist
+from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel
 
 import sys
 sys.path.append('../')
-sys.path.append('./')
 
 from config import BEST_WEIGHTS
 from config import MAX_KNN_BATCH as BATCH_SIZE
-from dataset import ModelNetDataLoader, ModelNet40Attack
+from dataset import ModelNet40NormalAttack
 from model import DGCNN, PointNetCls, PointNet2ClsSsg, PointConvDensityClsSsg
 from util.utils import str2bool, set_seed
-from attack import CWKNN, CWUKNN
-from attack import CrossEntropyAdvLoss, UntargetedLogitsAdvLoss
+from attack import CWKNN
+from attack import CrossEntropyAdvLoss, LogitsAdvLoss
 from attack import ChamferkNNDist
 from attack import ProjectInnerClipLinf
 
@@ -32,37 +30,40 @@ def attack():
     model.eval()
     all_adv_pc = []
     all_real_lbl = []
+    all_target_lbl = []
     num = 0
-    at_num, trans_num, total_num = 0, 0, 0
-    for pc, label in tqdm(test_loader):
+    for pc, label, target in tqdm(test_loader):
         with torch.no_grad():
             pc, label = pc.float().cuda(non_blocking=True), \
                 label.long().cuda(non_blocking=True)
-                
+            target_label = target.long().cuda(non_blocking=True)
+
         # attack!
-        best_pc, success_num = attacker.attack(pc, label)
+        best_pc, success_num = attacker.attack(pc, target_label)
 
         # results
         num += success_num
         all_adv_pc.append(best_pc)
         all_real_lbl.append(label.detach().cpu().numpy())
+        all_target_lbl.append(target_label.detach().cpu().numpy())
 
     # accumulate results
     all_adv_pc = np.concatenate(all_adv_pc, axis=0)  # [num_data, K, 3]
     all_real_lbl = np.concatenate(all_real_lbl, axis=0)  # [num_data]
-    return all_adv_pc, all_real_lbl, num
+    all_target_lbl = np.concatenate(all_target_lbl, axis=0)  # [num_data]
+    return all_adv_pc, all_real_lbl, all_target_lbl, num
 
 
 if __name__ == "__main__":
     # Training settings
     parser = argparse.ArgumentParser(description='Point Cloud Recognition')
     parser.add_argument('--data_root', type=str,
-                        default='official_data/modelnet40_normal_resampled')
+                        default='data/attack_data.npz')
     parser.add_argument('--model', type=str, default='pointnet', metavar='N',
                         choices=['pointnet', 'pointnet2',
                                  'dgcnn', 'pointconv'],
                         help='Model to use, [pointnet, pointnet++, dgcnn, pointconv]')
-    parser.add_argument('--feature_transform', type=str2bool, default=True,
+    parser.add_argument('--feature_transform', type=str2bool, default=False,
                         help='whether to use STN on features in PointNet')
     parser.add_argument('--dataset', type=str, default='mn40', metavar='N',
                         choices=['mn40', 'remesh_mn40', 'opt_mn40', 'conv_opt_mn40'])
@@ -79,21 +80,12 @@ if __name__ == "__main__":
                         help='Adversarial loss function to use')
     parser.add_argument('--kappa', type=float, default=15.,
                         help='min margin in logits adv loss')
-    parser.add_argument('--budget', type=float, default=0.1,
-                        help='clip budget')
     parser.add_argument('--attack_lr', type=float, default=1e-3,
                         help='lr in CW optimization')
     parser.add_argument('--num_iter', type=int, default=2500, metavar='N',
                         help='Number of iterations in each search step')
     parser.add_argument('--local_rank', default=-1, type=int,
                         help='node rank for distributed training')
-
-    parser.add_argument('--num_point', type=int, default=1024,
-                        help='num of points to use')
-    parser.add_argument('--use_normals', action='store_true', default=False, help='use normals')
-    parser.add_argument('--num_category', default=40, type=int, choices=[10, 40],  help='training on ModelNet10/40')
-    parser.add_argument('--use_uniform_sample', action='store_true', default=False, help='use uniform sampiling')
-    parser.add_argument('--process_data', action='store_true', default=False, help='save data offline')
     args = parser.parse_args()
     BATCH_SIZE = BATCH_SIZE[args.num_points]
     BEST_WEIGHTS = BEST_WEIGHTS[args.dataset][args.num_points]
@@ -101,7 +93,6 @@ if __name__ == "__main__":
         args.batch_size = BATCH_SIZE[args.model]
     set_seed(1)
     print(args)
-
 
     dist.init_process_group(backend='nccl')
     torch.cuda.set_device(args.local_rank)
@@ -131,33 +122,37 @@ if __name__ == "__main__":
         state_dict = {k[7:]: v for k, v in state_dict.items()}
         model.load_state_dict(state_dict)
 
+    # distributed mode on multiple GPUs!
+    # much faster than nn.DataParallel
     model = DistributedDataParallel(
         model.cuda(), device_ids=[args.local_rank])
-    # model = torch.nn.DataParallel(model).cuda()
 
     # setup attack settings
     if args.adv_func == 'logits':
-        adv_func = UntargetedLogitsAdvLoss(kappa=args.kappa)
+        adv_func = LogitsAdvLoss(kappa=args.kappa)
     else:
         adv_func = CrossEntropyAdvLoss()
     # hyper-parameters from their official tensorflow code
     dist_func = ChamferkNNDist(chamfer_method='adv2ori',
                                knn_k=5, knn_alpha=1.05,
                                chamfer_weight=5., knn_weight=3.)
-    clip_func = ProjectInnerClipLinf(budget=args.budget)
+    clip_func = ProjectInnerClipLinf(budget=0.1)
     attacker = CWKNN(model, adv_func, dist_func, clip_func,
                      attack_lr=args.attack_lr,
                      num_iter=args.num_iter)
 
     # attack
-    test_set = ModelNetDataLoader(root=args.data_root, args=args, split='test', process_data=args.process_data)
+    test_set = ModelNet40NormalAttack(args.data_root,
+                                      num_points=args.num_points,
+                                      normalize=True)
     test_sampler = DistributedSampler(test_set, shuffle=False)
-    test_loader = DataLoader(test_set, batch_size=args.batch_size, 
-                    shuffle=False, num_workers=10, 
-                    drop_last=False, sampler=test_sampler)
+    test_loader = DataLoader(test_set, batch_size=args.batch_size,
+                             shuffle=False, num_workers=4,
+                             pin_memory=True, drop_last=False,
+                             sampler=test_sampler)
 
     # run attack
-    attacked_data, real_label, success_num = attack()
+    attacked_data, real_label, target_label, success_num = attack()
 
     # accumulate results
     data_num = len(test_set)
@@ -170,9 +165,10 @@ if __name__ == "__main__":
         os.makedirs(save_path)
     if args.adv_func == 'logits':
         args.adv_func = 'logits_kappa={}'.format(args.kappa)
-    save_name = 'kNN-{}-{}-success_{:.4f}.npz'.\
-        format(args.model, args.budget,
-               success_rate)
+    save_name = 'kNN-{}-{}-success_{:.4f}-rank_{}.npz'.\
+        format(args.model, args.adv_func,
+               success_rate, args.local_rank)
     np.savez(os.path.join(save_path, save_name),
              test_pc=attacked_data.astype(np.float32),
-             test_label=real_label.astype(np.uint8))
+             test_label=real_label.astype(np.uint8),
+             target_label=target_label.astype(np.uint8))
