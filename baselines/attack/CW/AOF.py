34bc897b1462cacdfbc704c2c64905285688cb75
lbb <maths2games@gmail.com>
2022-01-09 12:40:42 +0800

    untargeted aof bugfix

--- baselines/attack/CW/AOF.py
+++ baselines/attack/CW/AOF.py
@@ -227,4 +227,4 @@ class CWAOF:
         print(preds.shape)
         success_num = (preds != target).sum().item()
         print('Successfully attack {}/{}'.format(success_num, B))
-        return o_bestdist, o_bestattack.transpose((0, 2, 1)), success_num
+        return o_bestdist, adv_pc.detach().cpu().numpy().transpose((0, 2, 1)), success_num
