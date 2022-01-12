aebca4929ccecbabd40750d5906ab7bb61fd21a1
liubinbin <1650476944@qq.com>
2022-01-12 16:33:11 +0800

    bugfix

--- baselines/defense.sh
+++ baselines/defense.sh
@@ -3,6 +3,7 @@
 data_folder=$1
 prefix=$2
 victim=$3
+gpuid=$4
 
 data="${data_folder}/${prefix}-concat.npz"
 # echo $data
@@ -11,28 +12,39 @@ defense_types=("srs" "dup" "sor")
 for i in ${!defense_types[@]}; do
     tp=${defense_types[$i]}
     echo "$tp defense and inference"
-    CUDA_VISIBLE_DEVICES=4 python defend_npz.py --data_root=$data --defense=$tp
+    CUDA_VISIBLE_DEVICES=$gpuid python defend_npz.py --data_root=$data --defense=$tp
     tp_folder="$data_folder/$tp"
     tp_prefix="${tp}_${prefix}"
     # echo $tp_folder
     # echo $tp_prefix
-    ./inf.sh 4 $tp_folder $tp_prefix $victim
+    ./inf.sh $gpuid $tp_folder $tp_prefix $victim
 done
 
 echo "ConvOnet Defense and inference"
-CUDA_VISIBLE_DEVICES=4 python ../ConvONet/opt_defense.py --sample_npoint=1024 --train=False --rep_weight=500.0 --data_root=$data
+cd ../ConvONet
+CUDA_VISIBLE_DEVICES=$gpuid python ../ConvONet/opt_defense.py --sample_npoint=1024 --train=False --rep_weight=500.0 --data_root=$data
+tp=convonet_opt
 tp_folder="$data_folder/ConvONet-Opt"
-tp_prefix="$convonet_opt-$prefix"
-./inf.sh 4 $tp_folder $tp_prefix $victim
+tp_prefix="${tp}-${prefix}"
+echo $tp_prefix
+echo $tp_folder
+cd ../baselines
+./inf.sh $gpuid $tp_folder $tp_prefix $victim
 
+cd ../ONet
 echo "Onet-Remesh Defense and inference"
-CUDA_VISIBLE_DEVICES=4 python ../ONet/remesh_defense.py --sample_npoint=1024 --train=False --data_root=$data
+CUDA_VISIBLE_DEVICES=$gpuid python ../ONet/remesh_defense.py --sample_npoint=1024 --train=False --data_root=$data
+tp=onet_remesh
 tp_folder="$data_folder/ONet-Mesh"
-tp_prefix="$onet_remesh-$prefix"
-./inf.sh 4 $tp_folder $tp_prefix $victim
+tp_prefix="${tp}-${prefix}"
+cd ../baselines
+./inf.sh $gpuid $tp_folder $tp_prefix $victim
 
+cd ../ONet
 echo "Onet-Opt Defense and inference"
-CUDA_VISIBLE_DEVICES=4 python ../ONet/opt_defense.py --sample_npoint=1024 --train=False --rep_weight=500.0 --data_root=$data
+CUDA_VISIBLE_DEVICES=$gpuid python ../ONet/opt_defense.py --sample_npoint=1024 --train=False --rep_weight=500.0 --data_root=$data
+tp=onet_opt
 tp_folder="$data_folder/ONet-Opt"
-tp_prefix="$onet_opt-$prefix"
-./inf.sh 4 $tp_folder $tp_prefix $victim
+tp_prefix="${tp}-${prefix}"
+cd ../baselines
+./inf.sh $gpuid $tp_folder $tp_prefix $victim
